64bfe30faa3f2fba04cbc1f6eb7cec425050b452
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 08:30:59 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Collaborative Notes App</h1>
+      <p>Welcome to the collaborative notes platform!</p>
+    </div>
+  );
+}
+
+export default App;
