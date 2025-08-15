2c4797f05411f14ae70e000cd9424562c490dc95
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 08:31:04 -0500

    feat: initial commit for server/index.js

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
